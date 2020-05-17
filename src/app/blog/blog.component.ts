16241f9d84d150180eca5caba15393288ea8f133
ivangm13 <ivangarcia346@gmail.com>
2020-05-17 18:30:34 +0200

    Realizada toda la funcionalidad básica de la aplicación. A falta aún de pulir detalles, mejorar entorno grafico, depurar codigo en temas como el filtro por categorias y añadir funcionalidades extra

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { PostsService } from '../services/posts.service';
+import { Post } from '../models/post.model';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-blog',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog.component.css']
 })
 export class BlogComponent implements OnInit {
+  arrPosts: Post[];
 
-  constructor() { }
+  constructor(private postsService: PostsService, private router: Router) {
 
-  ngOnInit(): void {
+  }
+
+  async  ngOnInit() {
+    this.arrPosts = await this.postsService.getAllPosts();
+  }
+  async filtrarCategoria($event) {
+    console.log($event.target.value)
+    this.arrPosts = await this.postsService.getPostsByCategoria($event.target.value)
+  }
+
+  goToNew() {
+  this.router.navigate(['/new']);
   }
 
 }
--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from '../models/post.model';
+import { PostsService } from '../services/posts.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-formulario',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent implements OnInit {
+  post: Post;
 
-  constructor() { }
+
+
+  constructor(private postsService: PostsService, private router: Router) {
+    this.post = new Post('', '', '', '', '', '');
+
+  }
 
   ngOnInit(): void {
+
+  }
+  onClickEnviar() {
+    console.log(this.post);
+    let imagen: string;
+    switch (this.post.categoria) {
+      case 'deportes':
+        imagen = 'https://educationatw.com/wp-content/uploads/2017/11/SPORTS-ICON-300x300.png';
+        break;
+      case 'actualidad':
+        imagen = 'https://previews.123rf.com/images/yupiramos/yupiramos1705/yupiramos170502477/77302211-papel-de-noticias-icono-aislado-dise%C3%B1o-de-ilustraci%C3%B3n-vectorial.jpg';
+        break;
+      case 'recetas':
+        imagen = 'https://1.bp.blogspot.com/-dvPMDBaKUfA/Vow-xtFYdTI/AAAAAAAAHh8/lEDKD3ODiMg/s1600/icono%2Bgastronomia.png';
+        break;
+      case 'tecnologia':
+        imagen = 'https://triaformacion.com/wp-content/uploads/2018/04/icono-tecnologia.jpg';
+        break;
+    }
+    this.post.imagen = imagen;
+    this.postsService.agregarPost(this.post);
+    this.router.navigate(['/blog']);
+
   }
 
 }
